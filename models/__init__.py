2ef74c50ad087008f544be53e30f402313467363
Khoale1096 <Khoale1096>
2020-11-22 09:22:41 -0500

    Add new learned pos emb

--- models/__init__.py
+++ models/__init__.py
@@ -3,8 +3,12 @@ Initialize the models module
 '''
 from models.transformer import Transformer
 from models.new_transformer import NewTransformer
+from models.single_learned_pos_emb_transformer import SingleLearnedPosEmbTransformer
+from models.multi_learned_pos_emb_transformer import MultiLearnedPosEmbTransformer
 
 MODELS = {
     'transformer': Transformer,
     'new_transformer': NewTransformer,
+    'single_learned_pos_emb_transformer': SingleLearnedPosEmbTransformer,
+    'multi_learned_pos_emb_transformer': MultiLearnedPosEmbTransformer,
 }
